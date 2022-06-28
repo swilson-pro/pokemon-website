568e5a9349952a0c51731107c3efb35a3266dd58
Will <swilson.carlson@hotmail.com>
2022-06-28 13:51:05 -0400

    set up the id to pic

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const pokemon = [
     {name: 'Pikachu', id: '025'}
 ]
 
+const userRoster = []
+
 const containerDiv = document.querySelector('#container')
 const newBtn = document.querySelector('#new-pokemon-btn')
 const rosterDiv = document.querySelector('#roster')
@@ -12,6 +14,14 @@ const rosterDiv = document.querySelector('#roster')
 newBtn.addEventListener('click', () => {
     let  num = prompt('ENTER A POKEMON NUMBER')
     console.log('Number Entered:', num)
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${(num)}.png`
+    let img = document.createElement('img')
+    img.setAttribute('src', imgUrl)
+    img.setAttribute('class', 'roster-img')
+    let position = document.querySelector(`#pokemon-${userRoster.length + 1}`)
+    position.append(img)
+    userRoster.push(num)
+
 })
 
 // DOM = Document Object Model
